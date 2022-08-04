392c840b6c8b202fbe329deb575c3324fd6bdf50
Tom-Shorter <tom.shorter1@gmail.com>
2022-08-04 12:03:35 +0100

    removal of dev testing code

--- beacon/request/validate_filters.py
+++ beacon/request/validate_filters.py
@@ -7,11 +7,4 @@ LOG = logging.getLogger(__name__)
 def validate_filters(json_body=None):
     supported = [filter["id"] for filter in get_filtering_terms()]
     supplied = [filter["id"] for filter in json_body["query"]["filters"]]
-    LOG.debug(supplied)
-    LOG.debug(supported)
-    LOG.debug(set(supplied) - set(supported))
-    if set(supplied) - set(supported) != set():
-        LOG.debug("False")
-    else:
-        LOG.debug("True")
     return set(supplied) - set(supported)
